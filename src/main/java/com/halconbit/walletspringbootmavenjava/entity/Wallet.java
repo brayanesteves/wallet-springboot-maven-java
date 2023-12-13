7152c6b4df21292403c397e5ed5e354a54adf949
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-12 23:28:15 -0400

    Entity - Wallet [12/12/2023]

---
+++ src/main/java/com/halconbit/walletspringbootmavenjava/entity/Wallet.java
@@ -0,0 +1,31 @@
+package com.halconbit.walletspringbootmavenjava.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="0_Wllt")
+public class Wallet implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="Rfrnc")
+    private Long reference;
+    @Column(name="Nm")
+    private String name;
+    @Column(name="AccntNmbr")
+    private String accountManager;
+    @Column(name="Dscrptn")
+    private String description;
+    @Column(name="Prrty")
+    private String priority; // 1=High; 2=Medium; 3=Low
+    @Column(name="CrrntBlnc")
+    private Double currentBalance;
+}
