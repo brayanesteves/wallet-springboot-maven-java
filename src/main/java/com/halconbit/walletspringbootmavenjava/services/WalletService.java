0ad629b7905003f998033648e14e3168c666509e
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-13 12:27:50 -0400

    Layer[Service] - WalletService [13/12/2023]

---
+++ src/main/java/com/halconbit/walletspringbootmavenjava/services/WalletService.java
@@ -0,0 +1,25 @@
+package com.halconbit.walletspringbootmavenjava.services;
+
+import com.halconbit.walletspringbootmavenjava.entity.Wallet;
+import com.halconbit.walletspringbootmavenjava.repository.WalletRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WalletService {
+
+    @Autowired
+    private WalletRepository walletRepository;
+
+    public Wallet createOrUpdate(Wallet wallet) {
+        if(wallet.getReference() == null) {
+            this.walletRepository.save(wallet);
+        } else {
+            this.walletRepository.save(wallet);
+        }
+        return wallet;
+    }
+
+}
