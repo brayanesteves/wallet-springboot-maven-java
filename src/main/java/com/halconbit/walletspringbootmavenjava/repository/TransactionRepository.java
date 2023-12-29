3591926367ccda9ca1a9f2909386630cab87d092
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-28 22:46:55 -0400

    Transaction - Transaction.java 'Refactor' [28/12/22023]

--- src/main/java/com/halconbit/walletspringbootmavenjava/repository/TransactionRepository.java
+++ src/main/java/com/halconbit/walletspringbootmavenjava/repository/TransactionRepository.java
@@ -1,9 +1,17 @@
 package com.halconbit.walletspringbootmavenjava.repository;
 
 import com.halconbit.walletspringbootmavenjava.entity.Transaction;
+import com.halconbit.walletspringbootmavenjava.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    Optional<Transaction> findByReference(Long reference);
+    List<Transaction> findByWallet(Wallet wallet);
+
 }
