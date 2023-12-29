593f4975af70b9ea4af421bb0a7ace25d8db0919
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-28 21:56:16 -0400

    Repository - TransactionRepository.java 'Add @Repository' [28/12/22023]

--- src/main/java/com/halconbit/walletspringbootmavenjava/repository/TransactionRepository.java
+++ src/main/java/com/halconbit/walletspringbootmavenjava/repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package com.halconbit.walletspringbootmavenjava.repository;
 
 import com.halconbit.walletspringbootmavenjava.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
